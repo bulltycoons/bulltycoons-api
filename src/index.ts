d6d48b3507c354007164efbd41b9e532e6bde504
BullTycoons <community@bulltycoons.com>
2021-10-27 02:44:21 +0200

    added metaverse

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,25 @@ const app = express();
 const port = 1234; // default port to listen
 
 // define a route handler for the default home page
-app.get( "/", ( req, res ) => {
-    res.send( "Hello world!" );
-} );
+app.get( "/healthcheck", ( req, res ) => {
+    res.send( "Perfectly working!" );
+});
+
+app.get("/metaverse/:id", (req, res) => {
+    res.header("Content-Type",'application/json');
+    const id = req.params.id;
+    try {
+        const verse = require(`../metaverses/${id}.json`);
+        res.status(200);
+        res.send(verse);
+    } catch (err) {
+        res.status(404);
+        res.send({ message: "Not found" });
+    }
+});
 
 // start the Express server
 app.listen( port, () => {
     // tslint:disable-next-line:no-console
     console.log( `server started at http://localhost:${ port }` );
-} );
+});
