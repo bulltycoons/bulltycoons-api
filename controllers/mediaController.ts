ecc1691585edbcfb594b2fcc8db9dfbc6248166e
BullTycoons <community@bulltycoons.com>
2021-11-27 23:45:24 +0100

    updated the team and image routes

---
+++ controllers/mediaController.ts
@@ -0,0 +1,29 @@
+import { NextFunction, Request, Response } from 'express';
+import axios from 'axios';
+import path from 'path';
+
+export const healthCheck = async (req:Request, res:Response, next:NextFunction) => {
+    res.send("Perfectly working!");
+}
+
+export const loadImageDeprecated = async (req:Request, res:Response, next:NextFunction) => {
+    const { id } = req.params;
+    const baseImageURI = "https://gateway.pinata.cloud/ipfs/QmZg5Pxb2yieDmyZP6PAQL5fzXZEyM8RGjob562FcsFgaW";
+
+    axios .get(`${baseImageURI}/${id}.png`, { responseType: 'arraybuffer' })
+    .then(response => {
+        res.type('png');
+        res.send(Buffer.from(response.data, "utf-8"));
+    })
+    .catch(err => {
+        console.log(err, "<== Loading File Error for id ", id);
+        res.status(404).send("Image not loaded");
+    });
+    
+}
+const dir = path.join(__dirname, 'assets')
+export const loadImage = async (req:Request, res:Response, next:NextFunction) => {
+    const { id } = req.params;
+    res.type('png');
+    res.sendFile(`${dir}/${id}.png`);
+}
